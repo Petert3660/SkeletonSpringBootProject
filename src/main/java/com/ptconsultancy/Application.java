39d75094f212cce89392ec45bda8c26ae77802ba
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-08-16 14:08:32 +0100

    master
    Update of this template to create a message service within

--- src/main/java/com/ptconsultancy/Application.java
+++ src/main/java/com/ptconsultancy/Application.java
@@ -1,7 +1,9 @@
 package com.ptconsultancy;
 
+import com.ptconsultancy.messages.MessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -20,6 +22,9 @@ public class Application implements CommandLineRunner {
 
     private static final int EXIT_STATUS = 0;
 
+    @Autowired
+    MessageHandler messageHandler;
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
             .headless(false)
@@ -28,6 +33,7 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
+        System.out.println(messageHandler.getMessage("messages.ptconsultancy.messages"));
         System.out.println("This is a working program - replace this code with your own code to create required Application!");
         System.out.println("EXITING NOW!");
         System.exit(EXIT_STATUS);
