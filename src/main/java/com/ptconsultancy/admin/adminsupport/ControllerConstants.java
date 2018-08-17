0764061623621797358017bc54683bbe38d15580
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-08-17 08:32:31 +0100

    master
    Addition of admin restcontroller section for universal admin

---
+++ src/main/java/com/ptconsultancy/admin/adminsupport/ControllerConstants.java
@@ -0,0 +1,8 @@
+package com.ptconsultancy.admin.adminsupport;
+
+public class ControllerConstants {
+
+    public static final int EXIT_STATUS = 0;
+    public static final String ID_KEY = "auth.admin.id";
+    public static final String PASS_KEY = "auth.admin.password";
+}
---
+++ src/main/java/com/ptconsultancy/admin/restcontrollers/AdminController.java
@@ -0,0 +1,32 @@
+package com.ptconsultancy.admin.restcontrollers;
+
+import com.ptconsultancy.admin.adminsupport.ControllerConstants;
+import com.ptconsultancy.messages.MessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    private MessageHandler messageHandler;
+
+    @Autowired
+    public AdminController(MessageHandler messageHandler) {
+        this.messageHandler = messageHandler;
+    }
+
+    @RequestMapping(path="/healthcheck", method= RequestMethod.GET)
+    public String healthcheck() {
+        return "Running OK";
+    }
+
+    @RequestMapping(path="/shutdown/{id}/{pass}", method=RequestMethod.POST)
+    public void shutdown(@PathVariable String id, @PathVariable String pass) {
+        if (id.equals(messageHandler.getMessage(ControllerConstants.ID_KEY)) && pass.equals(ControllerConstants.PASS_KEY)) {
+            System.exit(ControllerConstants.EXIT_STATUS);
+        }
+    }
+}
