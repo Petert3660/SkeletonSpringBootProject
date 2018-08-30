bff893d7169e4588d117d0652cb0badc0ed47d18
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-08-30 14:33:44 +0100

    master
    Update of ability to disable adminId and adminPassword return from services

--- src/main/java/com/ptconsultancy/admin/restcontrollers/AdminController.java
+++ src/main/java/com/ptconsultancy/admin/restcontrollers/AdminController.java
@@ -5,6 +5,7 @@ import com.ptconsultancy.admin.adminsupport.ControllerConstants;
 import com.ptconsultancy.admin.security.SecurityTokenManager;
 import com.ptconsultancy.messages.MessageHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,14 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
+    private static final String ADMINID_TOGGLE = "adminid.allowed";
+    private static final String ADMINPASS_TOGGLE = "adminpass.allowed";
+
+    private static final String CREDS_DISABLED_MESSAGE = "This facility has been disabled";
+
     private MessageHandler messageHandler;
 
     private SecurityTokenManager securityTokenManager;
 
+    private Environment env;
+
+    private boolean adminidToggle = false;
+    private boolean adminpassToggle = false;
+
     @Autowired
-    public AdminController(MessageHandler messageHandler, SecurityTokenManager securityTokenManager) {
+    public AdminController(MessageHandler messageHandler, SecurityTokenManager securityTokenManager, Environment env) {
         this.messageHandler = messageHandler;
         this.securityTokenManager = securityTokenManager;
+        this.env = env;
+        if (this.env.getProperty(ADMINID_TOGGLE).equals("true")) {
+            adminidToggle = true;
+        }
+        if (this.env.getProperty(ADMINPASS_TOGGLE).equals("true")) {
+            adminpassToggle = true;
+        }
     }
 
     @RequestMapping(path="/healthcheck", method=RequestMethod.GET)
@@ -54,19 +72,27 @@ public class AdminController {
 
     @RequestMapping(path="getadminid/{token}", method=RequestMethod.GET)
     public String getAdminId(@PathVariable String token) {
-        if (token.equals(securityTokenManager.getValueWithReset())) {
-            return messageHandler.getMessage(ControllerConstants.ID_KEY);
+        if (adminidToggle) {
+            if (token.equals(securityTokenManager.getValueWithReset())) {
+                return messageHandler.getMessage(ControllerConstants.ID_KEY);
+            } else {
+                return ControllerConstants.NO_TOKEN_MESSAGE;
+            }
         } else {
-            return ControllerConstants.NO_TOKEN_MESSAGE;
+            return CREDS_DISABLED_MESSAGE;
         }
     }
 
     @RequestMapping(path="getadminpass/{token}", method=RequestMethod.GET)
     public String getAdminPass(@PathVariable String token) {
-        if (token.equals(securityTokenManager.getValueWithReset())) {
-            return messageHandler.getMessage(ControllerConstants.PASS_KEY);
+        if (adminpassToggle) {
+            if (token.equals(securityTokenManager.getValueWithReset())) {
+                return messageHandler.getMessage(ControllerConstants.PASS_KEY);
+            } else {
+                return ControllerConstants.NO_TOKEN_MESSAGE;
+            }
         } else {
-            return ControllerConstants.NO_TOKEN_MESSAGE;
+            return CREDS_DISABLED_MESSAGE;
         }
     }
 }
