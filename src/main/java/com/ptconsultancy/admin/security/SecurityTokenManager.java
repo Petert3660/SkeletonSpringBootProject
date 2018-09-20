1c2ee2747ddc9d6f4ea978ce9a91fac80fd769ef
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-09-20 13:37:07 +0100

    master

--- src/main/java/com/ptconsultancy/admin/security/SecurityTokenManager.java
+++ src/main/java/com/ptconsultancy/admin/security/SecurityTokenManager.java
@@ -1,7 +1,7 @@
 package com.ptconsultancy.admin.security;
 
 import com.ptconsultancy.admin.adminsupport.ControllerConstants;
-import com.ptconsultancy.utilities.GenerateRandomKeys;
+import com.ptconsultancy.domain.utilities.GenerateRandomKeys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
