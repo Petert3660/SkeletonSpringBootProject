936f239d97215c9addc8f417eaab39d32f44529a
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-11-09 09:53:51 +0000

    master
    Further tests added

--- src/test/java/admin/restoperations/TestRestOperations.java
+++ src/test/java/admin/restoperations/TestRestOperations.java
@@ -7,9 +7,12 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ptconsultancy.admin.restoperations.AllServices;
 import com.ptconsultancy.admin.restoperations.RestOperations;
 import com.ptconsultancy.admin.restoperations.Service;
+import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
@@ -23,19 +26,25 @@ public class TestRestOperations {
     private static final String RETURNED_SECURITY_TOKEN = "a1b2c3";
     private static final String URL = PROTOCOL + TEST_SERVICE_NAME + STANDARD_SEPARATOR + TEST_ENDPOINT + STANDARD_SEPARATOR + RETURNED_SECURITY_TOKEN;
 
-    AllServices allServices = mock(AllServices.class);
-    Service service = mock(Service.class);
-    RestTemplate restTemplate = mock(RestTemplate.class);
-    RestOperations restOperations;
+    private AllServices allServices = mock(AllServices.class);
+    private Service service = mock(Service.class);
+    private RestTemplate restTemplate = mock(RestTemplate.class);
+    private RestOperations restOperations;
+    private String body;
+    private JsonNode bodyAsJson;
 
     @Before
-    public void setup() {
+    public void setup() throws IOException {
         restOperations = new RestOperations(allServices, restTemplate);
+        body = "{\n\"name\":\"Jim\"\n}";
+        ObjectMapper mapper = new ObjectMapper();
+        bodyAsJson = mapper.readTree(body);
         when(service.getName()).thenReturn(TEST_SERVICE_NAME);
         when(service.getUrl()).thenReturn(PROTOCOL + TEST_SERVICE_NAME);
         when(allServices.getServiceByName(TEST_SERVICE_NAME)).thenReturn(service);
         when(restTemplate.getForObject(URL, String.class)).thenReturn(RESPONSE);
         when(restTemplate.getForObject(PROTOCOL + TEST_SERVICE_NAME + STANDARD_SEPARATOR + SECURITY_TOKEN, String.class)).thenReturn(RETURNED_SECURITY_TOKEN);
+        when(restTemplate.postForObject(URL, bodyAsJson, String.class)).thenReturn(RESPONSE);
     }
 
     @Test
@@ -43,5 +52,22 @@ public class TestRestOperations {
         String response = restOperations.get(service, TEST_ENDPOINT, String.class);
 
         assertThat(response, is(RESPONSE));
+    }
+
+    @Test
+    public void test_post() throws IOException {
+        String response = restOperations.post(service, TEST_ENDPOINT, body);
+
+        assertThat(response, is(RESPONSE));
+    }
+
+    @Test
+    public void test_put() {
+
+    }
+
+    @Test
+    public void test_delete() {
+
     }
 }
